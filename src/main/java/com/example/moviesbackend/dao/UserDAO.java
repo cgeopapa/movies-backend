a2e6d5aa7abed2cedb449d545c82897c1c8194db
cgeopapa <cgeocodgod@gmail.com>
2020-07-03 21:52:26 +0300

    Added nativeQuery property for postgresql

--- src/main/java/com/example/moviesbackend/dao/UserDAO.java
+++ src/main/java/com/example/moviesbackend/dao/UserDAO.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserDAO extends JpaRepository<UserModel, Integer>
 {
-    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    @Query(value = "SELECT u FROM User u WHERE u.email = ?1", nativeQuery = true)
     UserModel findByEmail(String email);
 }

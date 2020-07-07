3e4a7cbb822c16d44ec063c8570628c96664066a
cgeopapa <cgeocodgod@gmail.com>
2020-07-08 01:09:34 +0300

    custom query .*

--- src/main/java/com/example/moviesbackend/dao/UserDAO.java
+++ src/main/java/com/example/moviesbackend/dao/UserDAO.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserDAO extends JpaRepository<UserModel, Integer>
 {
-    @Query(value = "SELECT u FROM user_model u WHERE u.email = ?1", nativeQuery = true)
+    @Query(value = "SELECT u.* FROM user_model u WHERE u.email = ?1", nativeQuery = true)
     UserModel findByEmail(String email);
 }
